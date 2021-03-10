efd73fb606be56d90db52586288be18c5d7fb64a
conagreen <icoqwee@gmail.com>
2021-03-10 14:33:17 +0900

    순차탐색, 이진탐색

--- Chapter2/algorithm/sparta_algorithm/week2/06_01_is_existing_target_number_sequential.py
+++ Chapter2/algorithm/sparta_algorithm/week2/06_01_is_existing_target_number_sequential.py
@@ -17,4 +17,4 @@ def is_existing_target_number_sequential(target, array):
 # 최악의 경우는 맨 끝까지 찾아야 되기 때문에 빅오 표기법으로 O(N)만큼의 시간 복잡도가 걸림.
 
 result = is_existing_target_number_sequential(finding_target, finding_numbers)
-print(result)  # True
+print(result)  # True 
