20c04957e96d8ac68cf88c5e85f58094cb0d8c3d
conagreen <icoqwee@gmail.com>
2021-03-10 14:14:37 +0900

    homework

--- Chapter0/sparta/homework/03_week/homework.py
+++ Chapter0/sparta/homework/03_week/homework.py
@@ -1,7 +1,7 @@
 # 지니뮤직의 1~50위 곡 스크래핑 (순위 / 곡 제목 / 가수)
 
-import requests
-from bs4 import BeautifulSoup
+import requests 
+from bs4 import BeautifulSoup 
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
