fd073e78e06826fc9d9465f8b0250dfd14273fd2
conagreen <icoqwee@gmail.com>
2021-03-15 22:29:50 +0900

    이해완료 후후

--- Chapter2/algorithm/chapter02-2/09_1932.py
+++ Chapter2/algorithm/chapter02-2/09_1932.py
@@ -5,7 +5,9 @@ n = int(input())    # 삼각형의 크기 -> 5
 t = []
 for i in range(n):  # 0 ~ 4 변수 i에 반복문을 통해 저장
     t.append(list(map(int, input().split()))) # 정수삼각형 만들기
+
 k = 2
+
 for i in range(1, n):   # i -> t(리스트)의 인덱스 (1 ~ 4) 
     for j in range(k):  # j -> 값의 인덱스 (0 ~ 1 / 0 ~ 2 / 0 ~ 3 / 0 ~ 4)
                                     # i:    1     2        3       4    
